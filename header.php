5c4a1c1e74bd84f5d5bb6496efb7d59afbc857ea
Mel Tajon <github@meltajon.com>
2012-12-09 14:08:22 -0800

    changed default menu depth to top level only

--- header.php
+++ header.php
@@ -120,7 +120,7 @@
 						'link_before'     => '',
 						'link_after'      => '',
 						// 'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-						'depth'           => 0,
+						'depth'           => 1,
 						// 'walker'          => ''
 						) 
 					); 
