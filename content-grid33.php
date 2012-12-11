3aacfefd0ff22d5af1dc50966ed1d8432eace202
Mel Tajon <meltajon@gmail.com>
2012-12-11 12:44:53 -0800

    removed dummy aside

--- content-grid33.php
+++ content-grid33.php
@@ -15,15 +15,3 @@
 	</footer><!-- #aside-footer -->
 	<?php endif; ?>
 </article><!-- .block -->
-
-<aside class="grid25 block">
-	<header class="aside-header">
-		<h1 class="aside-title title"><a href="#">Article Title</a></h1>
-	</header><!-- .aside-header -->
-	<div class="aside-body">
-		<p>In West Philadelphia, born and raised, on the playground is where I spent most of my days. Chillin' out back, relaxin', all cool. All shootin' some bball, outside of the school.</p>
-	</div><!-- .aside-body -->
-	<footer id="aside-footer">
-		<p class="read-more"><a href="#">Read more...</a></p>
-	</footer><!-- #aside-footer -->
-</aside><!-- .quarter -->
