3aab39340fdc015547283fa159ce807c2a5e794d
Mel Tajon <meltajon@gmail.com>
2012-12-11 14:45:50 -0800

    minor: revised copy for page title

--- 404.php
+++ 404.php
@@ -1,39 +1,37 @@
 <?php get_header(); ?>
 
-	<div id="primary">
-		<div id="content" role="main">
+	<div id="error-404" class="content">
 
-			<article id="post-0" class="post error404 not-found">
-				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'Well this is somewhat embarrassing, isn&rsquo;t it?', 'toolbox' ); ?></h1>
-				</header>
+		<article id="post-0" class="grid100 block">
+			<header id="error-404-header" class="entry-header">
+				<h1 id="error-404-title" class="entry-title"><?php _e( 'Page Not Found', 'toolbox' ); ?></h1>
+			</header>
 
-				<div class="entry-content">
-					<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'toolbox' ); ?></p>
+			<div id="error-404-body" class="entry-content">
+				<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'toolbox' ); ?></p>
 
-					<?php get_search_form(); ?>
+				<?php get_search_form(); ?>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+				<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
-					<div class="widget">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'toolbox' ); ?></h2>
-						<ul>
-						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
-						</ul>
-					</div>
+				<div class="widget">
+					<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'toolbox' ); ?></h2>
+					<ul>
+					<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
+					</ul>
+				</div>
 
-					<?php
-					/* translators: %1$s: smilie */
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'toolbox' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
+				<?php
+				/* translators: %1$s: smilie */
+				$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'toolbox' ), convert_smilies( ':)' ) ) . '</p>';
+				the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+				?>
 
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+				<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
-				</div><!-- .entry-content -->
-			</article><!-- #post-0 -->
+			</div><!-- .entry-content -->
+		</article><!-- #post-0 -->
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+	</div><!-- .content -->
 
 <?php get_footer(); ?>
