1f7e74a74328cf2bf5ddc921a195c08bbb64f02b
Mel Tajon <meltajon@gmail.com>
2012-12-11 16:23:29 -0800

    fixed orphan/invalid code from localization removal

--- content-grid100.php
+++ content-grid100.php
@@ -1,6 +1,6 @@
 <article id="post-<?php the_ID(); ?>" class="grid100 module block">
 	<header class="module-header">
-		<h1 class="module-title title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'toolbox' ), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a></h1>
+		<h1 class="module-title title"><a href="<?php the_permalink(); ?>" title="Full Post: <?php the_title_attribute('echo=0'); ?>"><?php the_title(); ?></a></h1>
 	</header><!-- .module-header -->
 	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
 	<div class="module-excerpt">
@@ -11,7 +11,7 @@
 		<?php the_content('Continue reading <span class="meta-nav">&rarr;</span>'); ?>
 	</div><!-- .module-body -->
 	<footer id="module-footer">
-		<p class="read-more"><a href="<?php the_permalink(); ?>" title="Full Post: <?php the_title_attribute( 'echo=0' ) ); ?>">Read more...</a></p>
+		<p class="read-more"><a href="<?php the_permalink(); ?>" title="Full Post: <?php the_title_attribute('echo=0'); ?>">Read more...</a></p>
 	</footer><!-- #module-footer -->
 	<?php endif; ?>
 </article><!-- .block -->
