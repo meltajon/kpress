bfe95f0f1c6b75cafcb27915a15bc70f0fa774af
Mel Tajon <meltajon@gmail.com>
2012-12-12 16:37:22 -0800

    added thumbnails to each module

--- content-grid25.php
+++ content-grid25.php
@@ -1,5 +1,11 @@
 <article id="post-<?php the_ID(); ?>" class="grid25 module block">
 	<header class="module-header">
+		<a class="module-thumb" href="<?php the_permalink(); ?>" title="Full Post: <?php the_title_attribute('echo=0'); ?>">
+			<?php if(has_post_thumbnail()) {
+				echo the_post_thumbnail('thumb');
+				}
+			?>
+		</a>
 		<h1 class="module-title title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'toolbox' ), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a></h1>
 	</header><!-- .module-header -->
 	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
