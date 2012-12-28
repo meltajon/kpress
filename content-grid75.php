61ff22920e3eb2cddc3b07bf059054e3cd9e7848
Mel Tajon <meltajon@gmail.com>
2012-12-27 16:05:40 -0800

    removed references to "Toolbox"

--- content-grid75.php
+++ content-grid75.php
@@ -6,7 +6,7 @@
 				}
 			?>
 		</a>
-		<h1 class="module-title title"><a class="title-link" href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'toolbox' ), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a></h1>
+		<h1 class="module-title title"><a class="title-link" href="<?php the_permalink(); ?>" title="Full Post: <?php the_title_attribute( 'echo=0' ); ?>"><?php the_title(); ?></a></h1>
 	</header><!-- .module-header -->
 	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
 	<div class="module-excerpt">
@@ -17,7 +17,7 @@
 		<?php the_content('Continue reading <span class="meta-nav">&rarr;</span>'); ?>
 	</div><!-- .module-body -->
 	<footer id="module-footer">
-		<p class="read-more"><a class="more-link" href="<?php the_permalink(); ?>" title="Full Post: <?php the_title_attribute( 'echo=0' ) ); ?>">Read more...</a></p>
+		<p class="read-more"><a class="more-link" href="<?php the_permalink(); ?>" title="Full Post: <?php the_title_attribute( 'echo=0' ); ?>">Read more...</a></p>
 	</footer><!-- #module-footer -->
 	<?php endif; ?>
 </article><!-- .block -->
